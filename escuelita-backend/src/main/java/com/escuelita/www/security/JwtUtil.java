2f098bfe7d8821af0e94c21346cb64ae0ee88273
BrayamAristaF <brayamaristafrndz@gmail.com>
2026-07-11 21:11:33 -0500

    fix: corregir inserts en data.sql, establecer clave estatica para JWT y limpiar archivos sql redundantes

--- escuelita-backend/src/main/java/com/escuelita/www/security/JwtUtil.java
+++ escuelita-backend/src/main/java/com/escuelita/www/security/JwtUtil.java
@@ -2,6 +2,10 @@ package com.escuelita.www.security;
 
 import java.util.Date;
 
+import io.jsonwebtoken.security.Keys;
+
+import java.nio.charset.StandardCharsets;
+
 import javax.crypto.SecretKey;
 
 import org.springframework.stereotype.Component;
@@ -10,19 +14,22 @@ import io.jsonwebtoken.Jwts;
 
 @Component
 public class JwtUtil {
-    private final SecretKey key = Jwts.SIG.HS256.key().build();
-    private final long EXPIRATION_TIME = 100L*365*24*60*60*1000;
+    private final SecretKey key = Keys.hmacShaKeyFor(
+            "Xy7$pQ2#mK9!aB5@zT8*wN4&eR1^vU6%cO3(kL0)jI7-hG8_fF9+dD0=sS1[aA2]"
+                    .getBytes(StandardCharsets.UTF_8));
+    private final long EXPIRATION_TIME = 100L * 365 * 24 * 60 * 60 * 1000;
 
-    public String generarToken(String clienteId){
+    public String generarToken(String clienteId) {
         return Jwts.builder()
-            .subject(clienteId)
-            .issuedAt(new Date())
-            .expiration(new Date(System.currentTimeMillis() 
-                    + EXPIRATION_TIME))
-            .signWith(key)
-            .compact();    
+                .subject(clienteId)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis()
+                        + EXPIRATION_TIME))
+                .signWith(key)
+                .compact();
     }
-    public boolean validarToken(String token){
+
+    public boolean validarToken(String token) {
         try {
             Jwts.parser().verifyWith(key).build()
                     .parseSignedClaims(token);
@@ -31,9 +38,9 @@ public class JwtUtil {
             return false;
         }
     }
-    
-    public String extraerClienteId(String token){
+
+    public String extraerClienteId(String token) {
         return Jwts.parser().verifyWith(key).build()
-                    .parseSignedClaims(token).getPayload().getSubject();
+                .parseSignedClaims(token).getPayload().getSubject();
     }
 }
